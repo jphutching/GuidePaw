53768806015da30c37768e89cf39b4480c8bc026
jphutching <jphutching@gmail.com>
2026-04-27 08:19:54 -0600

    Remove duplicate dashboard brand name

--- index.php
+++ index.php
@@ -83,7 +83,6 @@ $activeAlerts = $activeDog ? getDogAlertItems($pdo, $userId, (int) $activeDog['i
     <div class="gp-brand-hero">
         <img class="gp-brand-logo" src="/assets/brand/guidepaw-logo.png" alt="GuidePaw">
         <div class="gp-brand-copy">
-            <div class="gp-brand-name">GuidePaw</div>
             <div class="gp-brand-tagline">Training Trust for the Journey</div>
         </div>
     </div>
